fce4b1c0b11c7bfe48e094eba201d857eef98251
Kieran Warwick <kieran.warwick@thirdfort.com>
2022-08-03 09:00:08 +0100

    hello there !

--- app/src/components/intro.tsx
+++ app/src/components/intro.tsx
@@ -2,9 +2,10 @@ import { Component } from 'solid-js';
 
 const Intro: Component = () => (
   <div class="mb-6">
-    <h2 class="mt-6 text-4xl text-gray-700 font-bold capitalize dark:text-blue-200">
-      👋 Hello there!
-    </h2>
+    <div class="flex">
+      <img class="object-cover md:mx-auto grow" src="https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif" />
+    </div>
+
     <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
