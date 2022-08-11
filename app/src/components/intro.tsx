bca2f860438799b07d6ef82fed80586ab0f813dc
Kieran Warwick <kwarwick97@gmail.com>
2022-08-11 18:03:36 +0100

    prettier

--- app/src/components/intro.tsx
+++ app/src/components/intro.tsx
@@ -2,14 +2,22 @@ import { Component } from 'solid-js';
 
 const Intro: Component = () => (
   <div class="mb-6">
-    <div class="flex">
+    <div class="mt-4 flex">
       <img class="object-cover md:mx-auto grow" src="https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif" />
     </div>
 
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-2 dark:text-blue-200 text-justify">
       Software Engineer at Thirdfort, Master Computer Science Graduate from the University of Bath,
-      with First Class Honours.Open to new and interesting challenges, with opportunities for fast
-      growth and self-development. Enjoys contributing to real world problems, making the world
+      with First Class Honours.
+    </p>
+
+    <p class="my-2 dark:text-blue-200 text-justify">
+      Open to new and interesting challenges, with opportunities for fast
+      growth and self-development.
+    </p>
+
+    <p class="mb-4 mt-1 dark:text-blue-200 text-justify">
+      Enjoys contributing to real world problems, making the world
       better and more secure!
     </p>
   </div>
